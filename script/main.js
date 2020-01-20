7d32d23633201baaee00311acfa67f3618cefc4d
dyhalmeida <dyhalmeida@gmail.com>
2020-01-20 08:55:22 -0300

    Updates

---
+++ script/main.js
@@ -0,0 +1,56 @@
+// TabMenu
+const initTabMenu = () => {
+  const tabMenuList = document.querySelectorAll(".js-tabmenu li");
+  const tabContentList = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabMenuList.length && tabContentList.length) {
+    tabContentList[0].classList.add("active");
+    const activeTab = index => {
+      tabContentList.forEach(el => el.classList.remove("active"));
+      tabContentList[index].classList.add("active");
+    };
+
+    tabMenuList.forEach((el, index) => {
+      el.addEventListener("click", () => activeTab(index));
+    });
+  }
+};
+initTabMenu();
+
+// Accordion List
+const initAccordion = () => {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+
+  if (accordionList.length) {
+    accordionList[0].classList.add("active");
+    accordionList[0].nextElementSibling.classList.add("active");
+
+    function activeAccordion() {
+      this.classList.toggle("active");
+      this.nextElementSibling.classList.toggle("active");
+    }
+
+    accordionList.forEach(el => el.addEventListener("click", activeAccordion));
+  }
+};
+initAccordion();
+
+// Scroll Menu
+const initScrollMenu = () => {
+  const internalLinksList = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const section = document.querySelector(this.getAttribute("href"));
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  }
+
+  internalLinksList.forEach(el =>
+    el.addEventListener("click", scrollToSection)
+  );
+};
+
+initScrollMenu();
